d566b50bc934a77dc58146dc043cbd8c9d7b50fc
rsr986 <rsr986@gmail.com>
2023-02-24 15:41:53 +0530

    Initial Setup for vehicle-search project

---
+++ src/test/java/com/example/vehicle/api/search/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle.api.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
