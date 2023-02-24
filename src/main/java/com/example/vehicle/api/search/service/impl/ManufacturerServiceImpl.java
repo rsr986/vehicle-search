d07ebfdc6acfca40d795ee4dc4c7f0586cac7cef
rsr986 <rsr986@gmail.com>
2023-02-24 23:09:06 +0530

    added methods in controller and service to get Manufacturer by id

--- src/main/java/com/example/vehicle/api/search/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/com/example/vehicle/api/search/service/impl/ManufacturerServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.vehicle.api.search.service.ManufacturerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ManufacturerServiceImpl implements ManufacturerService {
@@ -24,5 +25,11 @@ public class ManufacturerServiceImpl implements ManufacturerService {
     @Override
     public List<Manufacturer> getAllManufacturers() {
         return manufacturerDAO.findAll();
+    }
+
+    @Override
+    public Manufacturer getManufacturerForId(int id) {
+        Optional<Manufacturer> manufacturer = manufacturerDAO.findById(id);
+        return manufacturer.orElseGet(() -> null);
     }
 }
